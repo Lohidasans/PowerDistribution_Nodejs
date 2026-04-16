dd2fcab124c0628fd1bc20f0dc1598d47767e31f
anitha.k <anitha.k@unitedhands.cc>
2026-04-16 14:32:43 +0530

    Changes in the seeders

--- seeders/20251029073122-states.js
+++ seeders/20251029073122-states.js
@@ -3,11 +3,11 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('states', [
-      { country_id: 1, state_code: 'TN', state_name: 'Tamil Nadu', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_code: 'KA', state_name: 'Karnataka', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_code: 'KL', state_name: 'Kerala', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_code: 'MH', state_name: 'Maharashtra', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_code: 'DL', state_name: 'Delhi', created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_code: 'TN', state_name: 'Tamil Nadu', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_code: 'KA', state_name: 'Karnataka', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_code: 'KL', state_name: 'Kerala', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_code: 'MH', state_name: 'Maharashtra', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_code: 'DL', state_name: 'Delhi', is_active: true, created_at: new Date(), updated_at: new Date() },
     ],
     { ignoreDuplicates: true } // prevents the unique constraint error
     );
--- seeders/20251029073128-districts.js
+++ seeders/20251029073128-districts.js
@@ -3,11 +3,11 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('districts', [
-      { country_id: 1, state_id: 1, short_name: 'CHN', district_name: 'Chennai', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_id: 1, short_name: 'CBE', district_name: 'Coimbatore', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_id: 1, short_name: 'MDU', district_name: 'Madurai', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_id: 1, short_name: 'TPR', district_name: 'Tirupur', created_at: new Date(), updated_at: new Date() },
-      { country_id: 1, state_id: 1, short_name: 'SLM', district_name: 'Salem', created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_id: 1, short_name: 'CHN', district_name: 'Chennai', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_id: 1, short_name: 'CBE', district_name: 'Coimbatore', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_id: 1, short_name: 'MDU', district_name: 'Madurai', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_id: 1, short_name: 'TPR', district_name: 'Tirupur', is_active: true, created_at: new Date(), updated_at: new Date() },
+      { country_id: 1, state_id: 1, short_name: 'SLM', district_name: 'Salem', is_active: true, created_at: new Date(), updated_at: new Date() },
     ],
     { ignoreDuplicates: true } // prevents the unique constraint error
     );
