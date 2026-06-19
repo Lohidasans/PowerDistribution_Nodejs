8ccff2623513ffe3232270096362472e3b58ab7c
anitha.k <anitha.k@unitedhands.cc>
2026-06-19 11:59:52 +0530

    Update customerSchemePaymentService.js

--- services/customerSchemePaymentService.js
+++ services/customerSchemePaymentService.js
@@ -279,6 +279,7 @@ try {
     e.status,
     s.scheme_name,
     s.id as scheme_id,
+    s.monthly_installments[1] AS monthly_installment_amount,
     inv.invoice_no,
     CASE
         WHEN e.completed_date IS NOT NULL
